4e1f98d9ea1c00739544a848427eaf87abe162b2
Krešimir Forjan <kresimirforjan5@gmail.com>
2021-05-24 22:42:25 +0200

    Add university model

---
+++ lib/models/university.dart
@@ -0,0 +1,13 @@
+import 'dart:developer';
+
+class University {
+  University({
+    required this.name,
+    required this.country,
+    required this.url,
+  });
+
+  final String name;
+  final String country;
+  final String url;
+}
